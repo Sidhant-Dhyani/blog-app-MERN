14e46ebbb0ee7c62779e89f71cf98c3ca804fd97
SidhantDhyani <siddhantydhyani99@gmail.com>
2023-05-02 23:02:26 +0530

    New Post Add Feature

--- api/index.js
+++ api/index.js
@@ -3,9 +3,14 @@ const app = express();
 const cors = require("cors");
 const bcrypt = require("bcrypt");
 const User = require("./models/User");
+const Post = require('./models/Post');
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const cookieParser = require('cookie-parser');
+const multer = require('multer');
+const uploadMiddleware = multer({dest: 'uploads'});
+const fs = require('fs');
+
 const salt = bcrypt.genSaltSync(10);
 const secret = "sidhant-blog";
 
@@ -68,11 +73,29 @@ app.get('/profile', (req, res) => {
     if(err) throw err;
     res.json(info);
   })
-  res.json(req.cookies);
+  // res.json(req.cookies);
 } );
- 
+
 app.post('/logout', (req, res) => {
   res.cookie('token', '').json('ok');
 })
+
+app.post('/post', uploadMiddleware.single('file'), async (req, res) => {
+  const {originalname, path} = req.file;
+  const parts = originalname.split('.');
+  const ext = parts[parts.length - 1];
+  const newPath = path+'.'+ext;
+  fs.renameSync(path, newPath);
+
+  const {title, summary, content} = req.body;
+  const postDoc = await Post.create({
+    title, 
+    summary,
+    content,
+    cover: newPath,
+  });
+
+  res.json(postDoc);
+});
 
 app.listen(3000);
